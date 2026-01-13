8a136746e3a09ee0b8b8ee206124c43671bc775f
Daniel Esteban <daniesteban.dev@gmail.com>
2026-01-13 00:53:20 -0300

    Primeiro controller para test feito

---
+++ src/main/java/dev/pantufa/CadastroDeSangue/Controller.java
@@ -0,0 +1,15 @@
+package dev.pantufa.CadastroDeSangue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/sejaBemVindo")
+    public String sejaBemVindo() {
+        return "<h1>Esta é minha primeira mensagem. Seja Bem-Vindo!</h1>";
+    }
+}
