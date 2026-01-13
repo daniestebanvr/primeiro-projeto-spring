05d389a80a585d01163a77fec29a76c952915b7c
Daniel Esteban <daniesteban.dev@gmail.com>
2026-01-12 21:40:25 -0300

    Primeiro projeto com Sping Boot inicializado

---
+++ src/main/java/dev/pantufa/CadastroDeSangue/CadastroDeSangueApplication.java
@@ -0,0 +1,12 @@
+package dev.pantufa.CadastroDeSangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeSangueApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/pantufa/CadastroDeSangue/CadastroDeSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pantufa.CadastroDeSangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
