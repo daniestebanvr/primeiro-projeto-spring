1fbb9bce06b9273d1a0b36aede5c35f7e3afce1f
Daniel Esteban <daniesteban.dev@gmail.com>
2026-01-14 21:37:10 -0300

    Model do doador criado e efetivado como uma entidade no BD

--- src/main/java/dev/pantufa/CadastroDeSangue/Model/DoadorModel.java
+++ src/main/java/dev/pantufa/CadastroDeSangue/Model/DoadorModel.java
@@ -1,12 +1,67 @@
 package dev.pantufa.CadastroDeSangue.Model;
 
 import dev.pantufa.CadastroDeSangue.Enums.TipoSanguineo;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
-@Entity
+@Entity //Essa anotação transforma uma classe em uma entidade no BD
+@Table(name = "tb_cadastro_de_doador")
 public class DoadorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDoador;
     private String nomeDoador;
     private Integer idadeDoador;
     private Long pesoDoador;
     private TipoSanguineo tipoSangue;
+
+    public DoadorModel(){
+    }
+
+    public DoadorModel(String nomeDoador, Integer idadeDoador, Long pesoDoador,TipoSanguineo tipoSangue){
+        this.nomeDoador = nomeDoador;
+        this.idadeDoador = idadeDoador;
+        this.pesoDoador = pesoDoador;
+        this.tipoSangue = tipoSangue;
+    }
+
+    public Long getIdDoador() {
+        return idDoador;
+    }
+
+    public void setIdDoador(Long idDoador) {
+        this.idDoador = idDoador;
+    }
+
+    public String getNomeDoador() {
+        return nomeDoador;
+    }
+
+    public void setNomeDoador(String nomeDoador) {
+        this.nomeDoador = nomeDoador;
+    }
+
+    public Integer getIdadeDoador() {
+        return idadeDoador;
+    }
+
+    public void setIdadeDoador(Integer idadeDoador) {
+        this.idadeDoador = idadeDoador;
+    }
+
+    public Long getPesoDoador() {
+        return pesoDoador;
+    }
+
+    public void setPesoDoador(Long pesoDoador) {
+        this.pesoDoador = pesoDoador;
+    }
+
+    public TipoSanguineo getTipoSangue() {
+        return tipoSangue;
+    }
+
+    public void setTipoSangue(TipoSanguineo tipoSangue) {
+        this.tipoSangue = tipoSangue;
+    }
 }
